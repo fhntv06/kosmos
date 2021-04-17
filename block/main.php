62e1fd9ea9a509e8aaa908a99b875c68231840ca
k-artem <k-artem@5th.ru>
2021-04-17 21:54:00 +0300

    1) вывод sidebar только на главной, 2) добавлено условие вывода section's на главную

--- block/main.php
+++ block/main.php
@@ -1,8 +1,13 @@
 
 
 <main class="main">
+    <?php
+        if( $url == "http://kosmos/" ){ 
 
-    <?php require_once "main/sidebar.php"; ?>
+            require_once "main/sidebar.php"; 
+
+        }
+    ?>
     
     <div id="container">
         <?php 
@@ -11,44 +16,42 @@
 
                 require_once "error.php";
 
-            }else{
+            }
 
-                // Логика для вывода статей по GET-параметрам   
-                if( $simbol_code ){
+            if( $url == "http://kosmos/" ){ 
+
+                require_once "main/news.php";
+                require_once "main/spaceships.php";
+                require_once "main/facts.php";
+
+            }
+            // Логика для вывода статей по GET-параметрам   
+            if( $simbol_code ){
         ?>
-                
-                    <section class="section"> 
-                        
-                        
-                        <div class="wrapper">
-                        <h2><?= $facts[0][0] ?></h2>
-                            <div class="wrapper__content">
-                               
-                                    <?php
-                                        $text = explode( ". ", $facts[0][3] ); 
-                                        $length = count($text);
-                                        for( $i = 0; $i < $length; $i++){
-                                    ?>
-                                        
-                                        <p><?= $text[$i] ?>.</p>
+            
+                <section class="section"> 
+                    
+                    
+                    <div class="wrapper">
+                    <h2><?= $facts[0][0] ?></h2>
+                        <div class="wrapper__content">
+                            
+                                <?php
+                                    $text = explode( ". ", $facts[0][3] ); 
+                                    $length = count($text);
+                                    for( $i = 0; $i < $length; $i++){
+                                ?>
+                                    
+                                    <p><?= $text[$i] ?>.</p>
 
-                                    <?php 
-                                        
-                                        } 
-                                    ?>
-                                
-                            </div>
+                                <?php 
+                                    
+                                    } 
+                                ?>
                         </div>
-                    </section>
-                
-        <?php
-                }else{
-
-                    require_once "main/news.php";
-                    require_once "main/spaceships.php";
-                    require_once "main/facts.php";
-
-                }
+                    </div>
+                </section>
+        <?php 
 
             }
         
