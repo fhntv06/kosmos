23968b300ba8c6060f1122ccd2209acc37858ee5
k-artem <k-artem@5th.ru>
2021-04-17 22:35:03 +0300

    исправлена ошибка в слове

--- common/head.php
+++ common/head.php
@@ -3,7 +3,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Space today блог в пгт. Промышленной  поддерживаемы одним разработчиком. NASA рекомендует, но это не точно.">
-    <meta name="keywords" content="NASA, SPACE, today, space today, Промышленная, блог о космосе, факты, удиаительные факты о космосе, космические аппараты">
+    <meta name="keywords" content="NASA, SPACE, today, space today, Промышленная, блог о космосе, факты, удивительные факты о космосе, космические аппараты">
     
     <link rel="shortcut icon" href="/files/img/logo.png" type="image/x-icon">
     
