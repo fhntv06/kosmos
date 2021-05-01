3d0e1d5a26abeeb27e64c6950a8e8f145c9b1c0c
k-artem <k-artem@5th.ru>
2021-05-02 02:23:05 +0300

    настроена передача контента со строниц навигации в main

--- about/index.php
+++ about/index.php
@@ -1,6 +1,18 @@
+
 <?php 
-$title = "О компании";
+
+$content = '
+
+<div class="section">
+    <h1>О компании</h1>
+    <h2>Наша компания создана ...</h2>
+</div>
+
+';
 
 require_once "../default.php";
 require_once "../index.php";
+
+?> 
+
 
