2df17f17f040e1511c2fd5341e8021c30ab82483
k-artem <k-artem@5th.ru>
2021-05-02 02:26:22 +0300

    реализована передача контента, добавлены комментарии

--- block/main.php
+++ block/main.php
@@ -12,12 +12,14 @@
     <div id="container">
         <?php 
         
+            // вывод контента для 404
             if( $url == $protocol."kosmos/404.php" ){ 
 
                 require_once "error.php";
 
             }
 
+            // вывод блоков на главной странице
             if( $url == $protocol."kosmos/" ){ 
 
                 require_once "main/news.php";
@@ -26,6 +28,7 @@
 
             }
             // Логика для вывода статей по GET-параметрам   
+            // вывод статей / публикации
             if( $simbol_code ){
         ?>
             
@@ -54,6 +57,23 @@
         <?php 
 
             }
+
+            if( $content != ""){
+
+                // переходы для навигации
+
+                echo $content;
+
+
+            }
+
+            // подтягиваем данный в из файла соответствующему пути и ссылки 
+                // $url = $_SERVER['REQUEST_URI'];
+                // $url = explode('?', $url);
+                // $url = $url[0];
+
+                // require_once "..".$url;
+
         
         ?>
     </div>
